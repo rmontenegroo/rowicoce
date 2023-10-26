d5bb3c319ea6b4b706ddf3645fb52be49e46f45b
rmontenegroo <montenegro.r@gmail.com>
2023-10-26 19:34:50 -0300

    Custom messages

--- RoWiCoCe/app/src/main/java/br/net/rodmonte/rowicoce/ui/custom/CustomFragment.kt
+++ RoWiCoCe/app/src/main/java/br/net/rodmonte/rowicoce/ui/custom/CustomFragment.kt
@@ -1,5 +1,7 @@
 package br.net.rodmonte.rowicoce.ui.custom
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +11,7 @@ import androidx.fragment.app.activityViewModels
 import br.net.rodmonte.rowicoce.RobotServiceViewModel
 import br.net.rodmonte.rowicoce.databinding.FragmentCustomBinding
 import android.util.Log
+import android.widget.Toast
 import androidx.core.widget.doAfterTextChanged
 
 class CustomFragment : Fragment() {
@@ -30,50 +33,100 @@ class CustomFragment : Fragment() {
     ): View {
         _binding = FragmentCustomBinding.inflate(inflater, container, false)
 
+        var custom1 = "custom1"
+        var custom2 = "custom2"
+        var custom3 = "custom3"
+        var custom4 = "custom4"
+        var custom5 = "custom5"
+        var custom6 = "custom6"
+        var custom7 = "custom7"
+
+        val sharedPref :SharedPreferences? = this.activity?.getSharedPreferences("br.net.rodmonte.rowicoce.preferences", Context.MODE_PRIVATE)
+        if (sharedPref != null) {
+            custom1 = sharedPref.getString("custom1_key", custom1) as String
+            custom2 = sharedPref.getString("custom2_key", custom2) as String
+            custom3 = sharedPref.getString("custom3_key", custom3) as String
+            custom4 = sharedPref.getString("custom4_key", custom4) as String
+            custom5 = sharedPref.getString("custom5_key", custom5) as String
+            custom6 = sharedPref.getString("custom6_key", custom6) as String
+            custom7 = sharedPref.getString("custom7_key", custom7) as String
+        }
+
+        binding.custom1Text.setText(custom1)
+        binding.custom2Text.setText(custom2)
+        binding.custom3Text.setText(custom3)
+        binding.custom4Text.setText(custom4)
+        binding.custom5Text.setText(custom5)
+        binding.custom6Text.setText(custom6)
+        binding.custom7Text.setText(custom7)
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.saveCustomCommandsButton.setOnClickListener {
+            val custom1 = binding.custom1Text.text.toString()
+            val custom2 = binding.custom2Text.text.toString()
+            val custom3 = binding.custom3Text.text.toString()
+            val custom4 = binding.custom4Text.text.toString()
+            val custom5 = binding.custom5Text.text.toString()
+            val custom6 = binding.custom6Text.text.toString()
+            val custom7 = binding.custom7Text.text.toString()
+
+            val sharedPref : SharedPreferences? = this.activity?.getSharedPreferences("br.net.rodmonte.rowicoce.preferences", Context.MODE_PRIVATE)
+
+            if (sharedPref == null) {
+                Toast.makeText(context, "An error has occurred saving data", Toast.LENGTH_SHORT).show()
+            } else {
+                with (sharedPref.edit()) {
+                    this.putString("custom1_key", custom1)
+                    this.putString("custom2_key", custom2)
+                    this.putString("custom3_key", custom3)
+                    this.putString("custom4_key", custom4)
+                    this.putString("custom5_key", custom5)
+                    this.putString("custom6_key", custom6)
+                    this.putString("custom7_key", custom7)
+                    this.apply()
+                }
+                Toast.makeText(context, "Data has been saved", Toast.LENGTH_SHORT).show()
+            }
+        }
+
         binding.custom1Text.doAfterTextChanged { text ->
             binding.custom1Button.isEnabled = text.toString() != ""
+            binding.saveCustomCommandsButton.isEnabled = text.toString() != ""
         }
 
         binding.custom2Text.doAfterTextChanged { text ->
             binding.custom2Button.isEnabled = text.toString() != ""
+            binding.saveCustomCommandsButton.isEnabled = text.toString() != ""
         }
 
         binding.custom3Text.doAfterTextChanged { text ->
             binding.custom3Button.isEnabled = text.toString() != ""
+            binding.saveCustomCommandsButton.isEnabled = text.toString() != ""
         }
 
         binding.custom4Text.doAfterTextChanged { text ->
             binding.custom4Button.isEnabled = text.toString() != ""
+            binding.saveCustomCommandsButton.isEnabled = text.toString() != ""
         }
 
         binding.custom5Text.doAfterTextChanged { text ->
             binding.custom5Button.isEnabled = text.toString() != ""
+            binding.saveCustomCommandsButton.isEnabled = text.toString() != ""
         }
 
         binding.custom6Text.doAfterTextChanged { text ->
             binding.custom6Button.isEnabled = text.toString() != ""
+            binding.saveCustomCommandsButton.isEnabled = text.toString() != ""
         }
 
         binding.custom7Text.doAfterTextChanged { text ->
             binding.custom7Button.isEnabled = text.toString() != ""
-        }
-
-        binding.custom8Text.doAfterTextChanged { text ->
-            binding.custom8Button.isEnabled = text.toString() != ""
-        }
-
-        binding.custom9Text.doAfterTextChanged { text ->
-            binding.custom9Button.isEnabled = text.toString() != ""
-        }
-
-        binding.custom10Text.doAfterTextChanged { text ->
-            binding.custom10Button.isEnabled = text.toString() != ""
+            binding.saveCustomCommandsButton.isEnabled = text.toString() != ""
         }
 
         binding.custom1Button.setOnClickListener {
@@ -131,34 +184,7 @@ class CustomFragment : Fragment() {
         }
 
         binding.custom7Button.setOnClickListener {
-            val message : String = binding.custom7Text.text.toString()
-            if (message != "") {
-                if (robotServiceViewModel.robotService.value?.sendMessage(message) == false) {
-                    Log.w(TAG, "Message has not been sent")
-                }
-            }
-        }
-
-        binding.custom8Button.setOnClickListener {
-            val message : String = binding.custom8Text.text.toString()
-            if (message != "") {
-                if (robotServiceViewModel.robotService.value?.sendMessage(message) == false) {
-                    Log.w(TAG, "Message has not been sent")
-                }
-            }
-        }
-
-        binding.custom9Button.setOnClickListener {
-            val message : String = binding.custom9Text.text.toString()
-            if (message != "") {
-                if (robotServiceViewModel.robotService.value?.sendMessage(message) == false) {
-                    Log.w(TAG, "Message has not been sent")
-                }
-            }
-        }
-
-        binding.custom10Button.setOnClickListener {
-            val message : String = binding.custom10Text.text.toString()
+            val message: String = binding.custom7Text.text.toString()
             if (message != "") {
                 if (robotServiceViewModel.robotService.value?.sendMessage(message) == false) {
                     Log.w(TAG, "Message has not been sent")
--- RoWiCoCe/app/src/main/java/br/net/rodmonte/rowicoce/ui/setup/SetupFragment.kt
+++ RoWiCoCe/app/src/main/java/br/net/rodmonte/rowicoce/ui/setup/SetupFragment.kt
@@ -1,5 +1,7 @@
 package br.net.rodmonte.rowicoce.ui.setup
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -33,7 +35,18 @@ class SetupFragment : Fragment() {
         _binding = FragmentSetupBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        robotServiceViewModel.setRobotService(RobotService(binding.editTextAddress.text.toString(), binding.editTextPort.text.toString().toInt()))
+        var useAddress = binding.editTextAddress.text.toString()
+        var usePort = binding.editTextPort.text.toString().toInt()
+
+        val sharedPref :SharedPreferences? = this.activity?.getSharedPreferences("br.net.rodmonte.rowicoce.preferences", Context.MODE_PRIVATE)
+        if (sharedPref != null) {
+            useAddress = sharedPref.getString("connection_address_key", useAddress) as String
+            usePort = sharedPref.getInt("connection_port_key", usePort)
+        }
+
+        binding.editTextAddress.setText(useAddress)
+        binding.editTextPort.setText(usePort.toString())
+        robotServiceViewModel.setRobotService(RobotService(useAddress, usePort))
 
         return root
     }
@@ -41,7 +54,29 @@ class SetupFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.saveConnectionDataButton.setOnClickListener {
+            val address = binding.editTextAddress.text.toString()
+            val port = binding.editTextPort.text.toString().toInt()
+
+            val sharedPref :SharedPreferences? = this.activity?.getSharedPreferences("br.net.rodmonte.rowicoce.preferences", Context.MODE_PRIVATE)
+
+            if (sharedPref == null) {
+                Toast.makeText(context, "An error has occurred saving data", Toast.LENGTH_SHORT).show()
+            } else {
+                with (sharedPref.edit()) {
+                    this.putString("connection_address_key", address)
+                    this.putInt("connection_port_key", port)
+                    this.apply()
+                }
+                Toast.makeText(context, "Data has been saved", Toast.LENGTH_SHORT).show()
+            }
+        }
+
         binding.editTextAddress.doAfterTextChanged {
+            binding.saveConnectionDataButton.isEnabled =
+                binding.editTextAddress.text.toString() != ""
+            binding.buttonTestConnection.isEnabled =
+                binding.editTextAddress.text.toString() != ""
             try {
                 robotServiceViewModel.setRobotService(RobotService(binding.editTextAddress.text.toString(), binding.editTextPort.text.toString().toInt()))
             } catch (e: Exception) {
@@ -50,6 +85,8 @@ class SetupFragment : Fragment() {
         }
 
         binding.editTextPort.doAfterTextChanged {
+            binding.saveConnectionDataButton.isEnabled = binding.editTextPort.text.toString() != ""
+            binding.buttonTestConnection.isEnabled = binding.editTextPort.text.toString() != ""
             try {
                 robotServiceViewModel.setRobotService(RobotService(binding.editTextAddress.text.toString(), binding.editTextPort.text.toString().toInt()))
             } catch (e: Exception) {
